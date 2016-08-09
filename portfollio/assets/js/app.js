d0aeb1fbe0db3164773e10ebac64a19b6460c7fe
Aaron <newcomer.aaron@icloud.com>
2016-08-09 14:45:43 -0400

    added updated projects

---
+++ portfollio/assets/js/app.js
@@ -0,0 +1,18 @@
+$(".about").click(function(div){
+  $('.mainContent').animate({
+  scrollTop: 0
+},500);
+})
+
+
+$(".myWork").click(function(div){
+  $('.mainContent').animate({
+  scrollTop: 635
+},500);
+})
+
+$(".contact").click(function(div){
+  $('.mainContent').animate({
+  scrollTop: 1100
+},1000);
+})
